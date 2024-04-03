7540002f603a685488a3b3091aab9b1c1352894d
IckyTheBiggy <elcia116eb@gmail.com>
2024-04-04 00:26:16 +0100

    A lot of changes to things

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set -x QT_QPA_PLATFORMTHEME "qt5ct"
 end
+
+function fish_greeting
+    neofetch
+end
+
 fish_add_path /home/BNN/.spicetify
